062a54882d869de1687e397949bedb7c9d69920e
Andrea Michelotti <Andrea.Michelotti@lnf.infn.it>
2013-11-14 13:55:14 +0100

    moved ui client, removed debug
    Change-Id: I95ffa5691a51deeadcbc96f5e1c5913ee57207e2

--- ocem_test_ui/test_ui_client.c
+++ ocem_test_ui/test_ui_client.c
@@ -5,74 +5,109 @@
 int main (int argc, char* argv[] )
 {
 
-    int err = 0;
-    char tmpInitString[256];
-    uint32_t devID;
-    const char *mdsServer = argv[1];
-    const char *devName = argv[2];
-    const char *attributeName = argv[3];
-    char * attributeValue = NULL;
+  int err = 0;
+  char tmpInitString[256];
+  uint32_t devID;
+  const char *mdsServer = argv[1];
+  const char *devName = argv[2];
+  const char *attributeName = argv[3];
+  char * attributeValue = NULL;
     
-    if(argc!=4){
-      printf("Usage is %s <MDS address> <CUname> <attribute name>\n",argv[0]);
-      return -1;
-    }
-    printf("using as MDS %s and CU %s\n",mdsServer,devName);
-        //create init string
-    sprintf(tmpInitString, "metadata-server=%s\nlog-on-console=true", mdsServer);
+  if(argc!=4){
+    printf("Usage is %s <MDS address> <CUname> <attribute name>\n",argv[0]);
+    return -1;
+  }
+  printf("using as MDS %s and CU %s\n",mdsServer,devName);
+  //create init string
+  //\nlog-on-console=true
+  sprintf(tmpInitString, "metadata-server=%s", mdsServer);
     
-        //init the toolkit
-    err = initToolkit(tmpInitString);
-    if (err != 0) {
-        printf("Error initToolkit %d\n", err);
-        return -1;
-    }
+  //init the toolkit
+  err = initToolkit(tmpInitString);
+  if (err != 0) {
+    printf("Error initToolkit %d\n", err);
+    return -1;
+  }
     
-    err = getNewControllerForDeviceID(devName, &devID);
-    if (err != 0) {
-        printf("Error getNewControllerForDeviceID %d\n", err);
-        return -1;
-    }
+  err = getNewControllerForDeviceID(devName, &devID);
+  if (err != 0) {
+    printf("Error getNewControllerForDeviceID %d\n", err);
+    return -1;
+  }
     
-    err = setControllerTimeout(devID, 10000);
-    if (err != 0) {
-        printf("Error setting timeout %d\n", err);
-        return -1;
-    }
+  err = setControllerTimeout(devID, 10000);
+  if (err != 0) {
+    printf("Error setting timeout %d\n", err);
+    return -1;
+  }
     
-    printf("Init device 0x%x\n",devID);
-    err = initDevice(devID);
-    if (err != 0) {
-        printf("Error initDevice %d\n", err);
-        return -1;
+  printf("Init device 0x%x\n",devID);
+  err = initDevice(devID);
+  if (err != 0) {
+    printf("Error initDevice %d\n", err);
+    return -1;
+  }
+  printf("Starting device 0x%x\n",devID);
+  err = startDevice(devID);
+  if (err != 0) {
+    printf("Error startDevice %d\n", err);
+    return -1;
+  }
+    
+  printf("Fetching device 0x%x\n",devID);
+  err = fetchLiveData(devID);
+  if (err != 0) {
+    printf("Error fetchLiveData %d\n", err);
+    return -1;
+  }
+      
+  err = getStrValueForAttribute(devID, attributeName, &attributeValue);
+  if (err != 0) {
+    printf("Error getStrValueForAttribute %d for value \"%s\"\n", err, attributeName);
+  }else {
+    if(attributeValue) {
+      printf("Got the value for %s with value %s\n", attributeName, attributeValue);
+      free(attributeValue);
     }
-    printf("Starting device 0x%x\n",devID);
-    err = startDevice(devID);
-    if (err != 0) {
-        printf("Error startDevice %d\n", err);
-        return -1;
-    }
-    printf("Fetching device 0x%x\n",devID);
-    err = fetchLiveData(devID);
-    if (err != 0) {
-        printf("Error fetchLiveData %d\n", err);
-        return -1;
-    }
-	
-    err = getStrValueForAttribute(devID, attributeName, &attributeValue);
-    if (err != 0) {
-        printf("Error getStrValueForAttribute %d for value %s\n", err, attributeName);
-    }else {
-        if(attributeValue) {
-            printf("Got the value for %s with value %s\n", attributeName, attributeValue);
-            free(attributeValue);
-        }
-    }
-    err = deinitToolkit();
-    if (err != 0) {
-        printf("Error deinitToolkit %d", err);
-        return -1;
-    }
-    return 0;
+  }
+      
+ 
+  // send on
+  // send setcurrent
+  sleep(1);
+  // send standby
+  printf("sending standby\n");
+  err = submitSlowControlCommand(devID,"{\"mode_type\":0}",
+				 1,
+				 50,
+				 0,
+				 0,
+				 0 ,
+				 0);
+  if(err!=0){
+    printf("## error submitting commands\n");
+    return -1;
+  }
+  sleep(1);
+  // send standby
+  printf("sending on\n");
+  err = submitSlowControlCommand(devID,"{\"mode_type\":1}",
+				 1,
+				 50,
+				 0,
+				 0,
+				 0 ,
+				 0);
+  if(err!=0){
+    printf("## error submitting commands\n");
+    return -1;
+  }
+  
+  err = deinitToolkit();
+  if (err != 0) {
+    printf("Error deinitToolkit %d", err);
+    return -1;
+  }
+  return 0;
 }
 
