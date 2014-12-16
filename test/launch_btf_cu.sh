317e0cef9894355ee20422c691eedff189d33a15
Andrea Michelotti <andrea.michelotti@lnf.infn.it>
2014-12-16 11:18:11 +0100

    test refactory, launch scripts removed, because replaced by generic chaos_launch_unit.sh

--- test/launch_btf_cu.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
- ocem_cu --log-level debug --log-on-console --metadata-server 192.168.143.21:5000 --sc_ocem_id QUATB004 --sc_driver OcemE642X:/dev/ttyr12,4 --sc_ocem_id QUATB003 --sc_driver OcemE642X:/dev/ttyr12,5 --sc_ocem_id DHSTB002 --sc_driver OcemE642X:/dev/ttyr12,6 >& cu1.log &
-
-ocem_cu --log-level debug --log-on-console --metadata-server 192.168.143.21:5000 --sc_ocem_id QUATB002 --sc_driver OcemE642X:/dev/ttyr14,11 --sc_ocem_id QUATB001 --sc_driver OcemE642X:/dev/ttyr14,9 --sc_ocem_id QUATB101 --sc_driver OcemE642X:/dev/ttyr14,3 --sc_ocem_id QUATB102 --sc_driver OcemE642X:/dev/ttyr14,0 --sc_ocem_id DHSTB001 --sc_driver OcemE642X:/dev/ttyr14,4 >& cu2.log &
--- test/launch_sim_cus.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-echo "Using metadata server $1"
-powersupply_cu --log-level debug --log-on-console --metadata-server $1:5000 --sc_ps_id SQUATB004 --sc_driver SimPSupply:/dev/ttyr12,4,100000:200000,200000:300000,100:10 --sc_ps_id SQUATB003 --sc_driver SimPSupply:/dev/ttyr12,5,100000:200000,200000:300000,100:10 --sc_ps_id SDHSTB002 --sc_driver SimPSupply:/dev/ttyr12,6,100000:200000,200000:300000,100:10 >& cu1.log &
-sleep 1
-powersupply_cu --log-level debug --log-on-console --metadata-server $1:5000 --sc_ps_id SQUATB002 --sc_driver SimPSupply:/dev/ttyr14,11,100000:200000,200000:300000,100:10 --sc_ps_id SQUATB001 --sc_driver SimPSupply:/dev/ttyr14,9,100000:200000,200000:300000,100:10 --sc_ps_id SQUATB101 --sc_driver SimPSupply:/dev/ttyr14,3,100000:200000,200000:300000,100:10 --sc_ps_id SQUATB102 --sc_driver SimPSupply:/dev/ttyr14,0,100000:200000,200000:300000,100:10 --sc_ps_id SDHSTB001 --sc_driver SimPSupply:/dev/ttyr14,4,100000:200000,200000:300000,100:10 >& cu2.log &
