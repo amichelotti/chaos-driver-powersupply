2460e0b1df708703740ac605400d48aa3660d9e0
Claudio Bisegni <Claudio.Bisegni@lnf.infn.it>
2017-10-24 17:25:40 +0200

    add all used opcode(perhaps)

--- models/ExternalDriver/ChaosPowerSupplyOpcodeLogic.cpp
+++ models/ExternalDriver/ChaosPowerSupplyOpcodeLogic.cpp
@@ -82,11 +82,11 @@ WRITE_ERR_ON_CMD(c, -2, "'err' key not found on external driver return package",
 }
 #define CHECK_KEY_IN_RESPONSE(r, k, t, e1, e2)\
 if(!r->hasKey(k)) {\
-    std::string es1 = CHAOS_FORMAT("'%1%' key is mandatory in remote driver response",%k);\
-    RETURN_ERROR(cmd, e1, es1.c_str(), __PRETTY_FUNCTION__);\
+std::string es1 = CHAOS_FORMAT("'%1%' key is mandatory in remote driver response",%k);\
+RETURN_ERROR(cmd, e1, es1.c_str(), __PRETTY_FUNCTION__);\
 } else if(!r->t(k) == 0) {\
-    std::string es2 = CHAOS_FORMAT("'%1%' key in remote driver response need to be int32 value", %k);\
-    RETURN_ERROR(cmd, e2, es2.c_str(), __PRETTY_FUNCTION__);\
+std::string es2 = CHAOS_FORMAT("'%1%' key in remote driver response need to be int32 value", %k);\
+RETURN_ERROR(cmd, e2, es2.c_str(), __PRETTY_FUNCTION__);\
 }
 
 int ChaosPowerSupplyOpcodeLogic::sendInit(DrvMsgPtr cmd) {
@@ -109,44 +109,125 @@ int ChaosPowerSupplyOpcodeLogic::sendDeinit(DrvMsgPtr cmd) {
     return cmd->ret;
 }
 
-int ChaosPowerSupplyOpcodeLogic::setPolarity(DrvMsgPtr cmd, int pol,uint32_t timeo_ms) {
-    return 0;
+int ChaosPowerSupplyOpcodeLogic::setPolarity(DrvMsgPtr cmd, int pol, uint32_t timeo_ms) {
+    CDWShrdPtr response;
+    CDWUniquePtr init_pack(new CDataWrapper());
+    CDWUniquePtr set_pola_pack(new CDataWrapper());
+    init_pack->addStringValue("opc", "set_pola");
+    set_pola_pack->addInt32Value("value", pol);
+    init_pack->addCSDataValue("par", *set_pola_pack);
+    SEND_REQUEST(cmd, init_pack, response);
+    if(response.get()){INFO << response->getJSONString();}
+    return cmd->ret;
 }
 
 int ChaosPowerSupplyOpcodeLogic::getPolarity(DrvMsgPtr cmd, int* pol,uint32_t timeo_ms){
-    return 0;
+    CDWShrdPtr response;
+    CDWUniquePtr init_pack(new CDataWrapper());
+    init_pack->addStringValue("opc", "get_pola");
+    SEND_REQUEST(cmd, init_pack, response);
+    if(response.get()){INFO << response->getJSONString();}
+    if(cmd->ret) {return cmd->ret;}
+    
+    CHECK_KEY_IN_RESPONSE(response, "value", isInt32Value, -1, -2);
+    *pol = response->getInt32Value("value");
+    return cmd->ret;
 }
 
 int ChaosPowerSupplyOpcodeLogic::setCurrentSP(DrvMsgPtr cmd, float current,uint32_t timeo_ms){
-    return 0;
+    CDWShrdPtr response;
+    CDWUniquePtr init_pack(new CDataWrapper());
+    CDWUniquePtr set_cur_pack(new CDataWrapper());
+    init_pack->addStringValue("opc", "set_cur");
+    set_cur_pack->addDoubleValue("value", current);
+    init_pack->addCSDataValue("par", *set_cur_pack);
+    SEND_REQUEST(cmd, init_pack, response);
+    if(response.get()){INFO << response->getJSONString();}
+    return cmd->ret;
 }
 
 int ChaosPowerSupplyOpcodeLogic::getCurrentSP(DrvMsgPtr cmd, float* current,uint32_t timeo_ms){
-    return 0;
+    CDWShrdPtr response;
+    CDWUniquePtr init_pack(new CDataWrapper());
+    init_pack->addStringValue("opc", "get_cur");
+    SEND_REQUEST(cmd, init_pack, response);
+    if(response.get()){INFO << response->getJSONString();}
+    if(cmd->ret) {return cmd->ret;}
+    
+    CHECK_KEY_IN_RESPONSE(response, "value", isDoubleValue, -1, -2);
+    *current = response->getDoubleValue("value");
+    return cmd->ret;
 }
 
 int ChaosPowerSupplyOpcodeLogic::startCurrentRamp(DrvMsgPtr cmd, uint32_t timeo_ms){
-    return 0;
+    CDWShrdPtr response;
+    CDWUniquePtr init_pack(new CDataWrapper());
+    init_pack->addStringValue("opc", "start_ramp");
+    SEND_REQUEST(cmd, init_pack, response);
+    if(response.get()){INFO << response->getJSONString();}
+    return cmd->ret;
 }
 
 int ChaosPowerSupplyOpcodeLogic::getVoltageOutput(DrvMsgPtr cmd, float* volt,uint32_t timeo_ms){
-    return 0;
+    CDWShrdPtr response;
+    CDWUniquePtr init_pack(new CDataWrapper());
+    init_pack->addStringValue("opc", "get_vol");
+    SEND_REQUEST(cmd, init_pack, response);
+    if(response.get()){INFO << response->getJSONString();}
+    if(cmd->ret) {return cmd->ret;}
+    
+    CHECK_KEY_IN_RESPONSE(response, "value", isDoubleValue, -1, -2);
+    *volt = response->getDoubleValue("value");
+    return cmd->ret;
 }
 
 int ChaosPowerSupplyOpcodeLogic::getCurrentOutput(DrvMsgPtr cmd, float* current,uint32_t timeo_ms){
-    return 0;
+    CDWShrdPtr response;
+    CDWUniquePtr init_pack(new CDataWrapper());
+    init_pack->addStringValue("opc", "get_cur");
+    SEND_REQUEST(cmd, init_pack, response);
+    if(response.get()){INFO << response->getJSONString();}
+    if(cmd->ret) {return cmd->ret;}
+    
+    CHECK_KEY_IN_RESPONSE(response, "value", isDoubleValue, -1, -2);
+    *current = response->getDoubleValue("value");
+    return cmd->ret;
 }
 
-int ChaosPowerSupplyOpcodeLogic::setCurrentRampSpeed(DrvMsgPtr cmd, float asup,float asdown,uint32_t timeo_ms){
-    return 0;
+int ChaosPowerSupplyOpcodeLogic::setCurrentRampSpeed(DrvMsgPtr cmd, float asup, float asdown, uint32_t timeo_ms){
+    CDWShrdPtr response;
+    CDWUniquePtr init_pack(new CDataWrapper());
+    CDWUniquePtr set_slope_pack(new CDataWrapper());
+    init_pack->addStringValue("opc", "set_slope");
+    set_slope_pack->addDoubleValue("up", asup);
+    set_slope_pack->addDoubleValue("uptown", asdown);
+    init_pack->addCSDataValue("par", *set_slope_pack);
+    SEND_REQUEST(cmd, init_pack, response);
+    if(response.get()){INFO << response->getJSONString();}
+    return cmd->ret;
 }
 
 int ChaosPowerSupplyOpcodeLogic::resetAlarms(DrvMsgPtr cmd, uint64_t alrm,uint32_t timeo_ms){
-    return 0;
+    CDWShrdPtr response;
+    CDWUniquePtr init_pack(new CDataWrapper());
+    CDWUniquePtr set_slope_pack(new CDataWrapper());
+    init_pack->addStringValue("opc", "rst_alarm");
+    SEND_REQUEST(cmd, init_pack, response);
+    if(response.get()){INFO << response->getJSONString();}
+    return cmd->ret;
 }
 
 int ChaosPowerSupplyOpcodeLogic::getAlarms(DrvMsgPtr cmd, uint64_t*alrm,uint32_t timeo_ms){
-    return 0;
+    CDWShrdPtr response;
+    CDWUniquePtr init_pack(new CDataWrapper());
+    init_pack->addStringValue("opc", "get_alarm");
+    SEND_REQUEST(cmd, init_pack, response);
+    if(response.get()){INFO << response->getJSONString();}
+    if(cmd->ret) {return cmd->ret;}
+    
+    CHECK_KEY_IN_RESPONSE(response, "value", isDoubleValue, -1, -2);
+    *alrm = response->getUInt64Value("value");
+    return cmd->ret;
 }
 
 int ChaosPowerSupplyOpcodeLogic::shutdown(DrvMsgPtr cmd, uint32_t timeo_ms){
@@ -171,6 +252,8 @@ int ChaosPowerSupplyOpcodeLogic::getState(DrvMsgPtr cmd, int* state,std::string&
     
     CHECK_KEY_IN_RESPONSE(response, "value", isInt32Value, -1, -2);
     CHECK_KEY_IN_RESPONSE(response, "description", isStringValue, -3, -4);
+    *state = response->getInt32Value("value");
+    desc = response->getStringValue("description");
     return cmd->ret;
 }
 
@@ -178,8 +261,17 @@ int ChaosPowerSupplyOpcodeLogic::getSWVersion(DrvMsgPtr cmd, std::string& versio
     return 0;
 }
 
-int ChaosPowerSupplyOpcodeLogic::getHWVersion(DrvMsgPtr cmd, std::string& version,uint32_t timeo_ms){
-    return 0;
+int ChaosPowerSupplyOpcodeLogic::getHWVersion(DrvMsgPtr cmd, std::string& version, uint32_t timeo_ms){
+    CDWShrdPtr response;
+    CDWUniquePtr init_pack(new CDataWrapper());
+    init_pack->addStringValue("opc", "get_hw_ver");
+    SEND_REQUEST(cmd, init_pack, response);
+    if(response.get()){INFO << response->getJSONString();}
+    if(cmd->ret) {return cmd->ret;}
+    
+    CHECK_KEY_IN_RESPONSE(response, "value", isStringValue, -1, -2);
+    version = response->getStringValue("value");
+    return cmd->ret;
 }
 
 int ChaosPowerSupplyOpcodeLogic::getCurrentSensibility(DrvMsgPtr cmd, float *sens){
@@ -207,7 +299,7 @@ int ChaosPowerSupplyOpcodeLogic::getMaxMinVoltage(DrvMsgPtr cmd, float*max,float
 }
 
 int ChaosPowerSupplyOpcodeLogic::getAlarmDesc(DrvMsgPtr cmd, uint64_t* alarm){
-   return 0;
+    return 0;
 }
 
 int ChaosPowerSupplyOpcodeLogic::forceMaxCurrent(DrvMsgPtr cmd, float max){
@@ -239,81 +331,130 @@ MsgManagmentResultType::MsgManagmentResult ChaosPowerSupplyOpcodeLogic::execOpco
             out->result = sendDeinit(cmd);
             break;
         }
-        case OP_SET_POLARITY:{
+        case OP_SET_POLARITY:
+            DBG<< "Set Polarity to:"<<in->ivalue<<" timeo:"<<in->timeout;
+            out->result= setPolarity(cmd, in->ivalue,in->timeout);
+            
             break;
-        }
-        case OP_GET_POLARITY:{
+        case OP_GET_POLARITY:
+            out->result = getPolarity(cmd, &out->ivalue,in->timeout);
+            DBG<< "Got Polarity "<<out->ivalue;
             break;
-        }
-        case OP_SET_SP:{//set current set point
+        case OP_SET_SP:
+            DBG<< "Set Current SP to:"<<in->fvalue0<<" timeo:"<<in->timeout;
+            out->result = setCurrentSP(cmd, in->fvalue0,in->timeout);
             break;
-        }
-        case OP_GET_SP:{
+            
+        case OP_FORCE_MAX_CURRENT:
+            DBG<< "Force max current"<<in->fvalue0;
+            out->result = forceMaxCurrent(cmd, in->fvalue0);
             break;
-        }
-        case OP_START_RAMP:{
+        case OP_FORCE_MAX_VOLTAGE:
+            DBG<< "Force max voltage"<<in->fvalue0;
+            out->result = forceMaxVoltage(cmd, in->fvalue0);
             break;
-        }
-        case OP_GET_VOLTAGE_OUTPUT:{
+        case OP_SET_CURRENT_SENSIBILITY:
+            DBG<< "Set current Sensibility"<<in->fvalue0;
+            out->result = setCurrentSensibility(cmd, in->fvalue0);
             break;
-        }
-        case OP_GET_CURRENT_OUTPUT:{
+        case OP_SET_VOLTAGE_SENSIBILITY:
+            DBG<< "Set voltage sensibility"<<in->fvalue0;
+            out->result = setVoltageSensibility(cmd, in->fvalue0);
             break;
-        }
-        case OP_SET_CURRENT_RAMP_SPEED:{
+            
+            
+        case OP_GET_SP: // get current set point
+            out->result = getCurrentSP(cmd, &out->fvalue0,in->timeout);
+            DBG<< "Got Current SP "<<out->fvalue0;
             break;
-        }
-        case OP_RESET_ALARMS:{
+        case OP_START_RAMP: // start ramp
+            DBG<< "Start Ramp timeo:"<<in->timeout;
+            out->result = startCurrentRamp(cmd, in->timeout);
             break;
-        }
-        case OP_GET_ALARMS:{
+        case OP_GET_VOLTAGE_OUTPUT:
+            out->result = getVoltageOutput(cmd, &out->fvalue0,in->timeout);
+            DBG<< "Got Voltage "<<out->fvalue0;
             break;
-        }
-        case OP_SHUTDOWN:{
+        case OP_GET_CURRENT_OUTPUT:
+            out->result = getCurrentOutput(cmd, &out->fvalue0,in->timeout);
+            DBG<< "Got Current "<<out->fvalue0;
             break;
-        }
-        case OP_STANDBY:{
+        case OP_SET_CURRENT_RAMP_SPEED:
+            DBG<<"Setting current ramp speed min:"<<in->fvalue0<<" max:"<<in->fvalue1<<" timeout:"<<in->timeout;
+            out->result = setCurrentRampSpeed(cmd, in->fvalue0,in->fvalue1,in->timeout);
             break;
-        }
-        case OP_POWERON:{
+        case OP_RESET_ALARMS:
+            DBG<<"Reset alarms to:"<<in->alarm_mask<<" timeout:"<<in->timeout;
+            out->result = resetAlarms(cmd, in->alarm_mask,in->timeout);
             break;
+        case OP_GET_ALARMS:
+            out->result = getAlarms(cmd, &out->alarm_mask,in->timeout);
+            DBG<<"Got alarms to:"<<out->alarm_mask<<" timeout:"<<in->timeout;
+            break;
+            
+        case OP_GET_FEATURE:{
+            uint64_t feat=getFeatures(cmd);
+            out->alarm_mask=feat;
+            DBG<<"Got Features:"<<feat;
         }
+            break;
+        case OP_SHUTDOWN:
+            DBG<<"Shutting down"<<" timeout:"<<in->timeout;
+            out->result = shutdown(cmd, in->timeout);
+            break;
+        case OP_STANDBY:
+            DBG<<"Standby "<<" timeout:"<<in->timeout;
+            out->result = standby(cmd, in->timeout);
+            break;
+        case OP_POWERON:
+            DBG<<"Poweron "<<" timeout:"<<in->timeout;
+            out->result = poweron(cmd, in->timeout);
+            break;
         case OP_GET_STATE:{
             std::string desc;
             out->result = getState(cmd, &out->ivalue,desc,in->timeout);
             strncpy(out->str,desc.c_str(),MAX_STR_SIZE);
+            DBG<<"Got State: "<<out->ivalue<<" \""<<desc<<"\" timeout:"<<in->timeout;
             break;
         }
         case OP_GET_SWVERSION:{
+            std::string ver;
+            out->result = getSWVersion(cmd, ver,in->timeout);
+            DBG<<"Got HW Version:\""<<ver<<"\" timeout:"<<in->timeout;
+            strncpy(out->str,ver.c_str(),MAX_STR_SIZE);;
             break;
         }
         case OP_GET_HWVERSION:{
+            std::string ver;
+            out->result = getHWVersion(cmd, ver,in->timeout);
+            DBG<<"Got SW Version:\""<<ver<<"\" timeout:"<<in->timeout;
+            strncpy(out->str,ver.c_str(),MAX_STR_SIZE);;
             break;
         }
-        case OP_GET_CURRENT_SENSIBILITY:{
+        case OP_GET_CURRENT_SENSIBILITY:
+            out->result = getCurrentSensibility(cmd, &out->fvalue0);
+            DBG<<"Got Current sensibility: \""<<out->fvalue0<<"\"";
             break;
-        }
-        case OP_SET_VOLTAGE_SENSIBILITY:{
+        case OP_GET_VOLTAGE_SENSIBILITY:
+            out->result = getVoltageSensibility(cmd, &out->fvalue0);
+            DBG<<"Got Voltage sensibility: \""<<out->fvalue0<<"\"";
             break;
-        }
-        case OP_GET_MAXMIN_CURRENT:{
+            
+        case OP_GET_MAXMIN_CURRENT:
+            out->result = getMaxMinCurrent(cmd, &out->fvalue0,&out->fvalue1);
+            DBG<<"Got Max "<<out->fvalue0<<" Min "<< out->fvalue1<<" current";
+            
             break;
-        }
-        case OP_GET_MAXMIN_VOLTAGE:{
+        case OP_GET_MAXMIN_VOLTAGE:
+            out->result = getMaxMinVoltage(cmd, &out->fvalue0,&out->fvalue1);
+            DBG<<"Got Max "<<out->fvalue0<<" Min "<< out->fvalue1<<" voltage";
             break;
-        }
-        case OP_GET_ALARM_DESC:{
+        case OP_GET_ALARM_DESC:
+            out->result = getAlarmDesc(cmd, &out->alarm_mask);
+            DBG<<"Got Alarm maxk "<<out->alarm_mask;
             break;
-        }
-        case OP_FORCE_MAX_CURRENT:{
-            break;
-        }
-        case OP_FORCE_MAX_VOLTAGE:{
-            break;
-        }
-        case OP_GET_FEATURE:{
-            break;
-        }
+        default:
+            ERR<<"Opcode not supported:"<<cmd->opcode;
     }
     return result;
 }
