d6a11d0af1c34faf2e71b05cc7850453f473a349
amichelo <andrea.michelotti@lnf.infn.it>
2020-03-13 17:55:04 +0100

    fixed bad parameters

--- core/SCPowerSupplyControlUnit.cpp
+++ core/SCPowerSupplyControlUnit.cpp
@@ -272,7 +272,7 @@ void ::driver::powersupply::SCPowerSupplyControlUnit::unitDefineActionAndDataset
 			"default driver timeout)",
 			DataType::TYPE_INT32,
 			DataType::Input);
-	addBinaryAttributeAsSubtypeToDataSet("conversionFactor", "Array of double Coefficents used to convert value from the CU actual units and the driver elementary units", chaos::DataType::SUB_TYPE_DOUBLE, 1, chaos::DataType::Input);
+	//addBinaryAttributeAsSubtypeToDataSet("conversionFactor", "Array of double Coefficents used to convert value from the CU actual units and the driver elementary units", chaos::DataType::SUB_TYPE_DOUBLE, 1, chaos::DataType::Input);
 
 /*	addAttributeToDataSet("resolution",
 			"Double Minimum meaningful variation of the set respect to the last accepted one",
